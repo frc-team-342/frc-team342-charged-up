545db12f75ab532eec21b3a2637e6c16510fe2cb
Max <Aidanpersonal2006@mail.com>
2023-01-17 18:34:13 -0500

    Code for pipeline change

---
+++ src/main/java/frc/robot/Limelight.java
@@ -0,0 +1,25 @@
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+class Limelight {
+
+    private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    public int getPipeline() {
+
+        return table.getEntry("pipeline").getNumber(0).intValue();
+
+    }
+
+    public void togglePipeline() {
+
+        int currPipe = getPipeline();
+        if (currPipe == 0) {
+            table.getEntry("pipeline").setNumber(1);
+        } else {
+            table.getEntry("pipeline").setNumber(0);
+        }
+
+    }
+}
